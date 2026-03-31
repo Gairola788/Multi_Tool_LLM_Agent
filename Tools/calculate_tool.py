1bf8bf7b14828945ffa302a21ef8ad48bfdbd909
Gairola788 <akshatgairola633@gmail.com>
2026-03-31 22:04:47 +0530

    doctring updated for all tools

--- Tools/calculate_tool.py
+++ Tools/calculate_tool.py
@@ -1,7 +1,7 @@
 from langchain.tools import tool
 
 @tool
-def cal_agent(expression):
+def cal_agent(expression) -> str:
     """"Calculate the expression including addition,subtraction,multiplication and Division,
          args : any expression for example : 5 + 3"""
     try:
