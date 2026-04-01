e92ccb17805969ebd00cf6f66c8f262ae6a252b3
Gairola788 <akshatgairola633@gmail.com>
2026-04-01 16:29:38 +0530

    Tool Definition Updated

--- Tools/calculate_tool.py
+++ Tools/calculate_tool.py
@@ -1,10 +1,16 @@
 from langchain.tools import tool
 
 
-@tool("Calculator")
-def cal_agent(expression) -> str:
-    """"Calculate the expression including addition,subtraction,multiplication and Division,
-         args : any expression for example : 5 + 3"""
+@tool
+def cal_agent(expression : str) -> str:
+    """Calculate the expression including addition,subtraction,multiplication and Division,
+         args : any expression for example : 5 + 3
+         
+          Args:
+        expression: A mathematical expression, for example '5 + 3' or '3000 + 2500 + 4000'
+    
+         Returns:
+        The calculated result"""
     try:
         result = eval(expression)
         return f"Result: {result}"
